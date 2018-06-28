3fcceb6aa65e377953681ccffc176646ca7f1143
va rathana <varathana15@kit.edu.kh>
2018-06-28 15:41:43 +0700

    dd

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
+const app = express()
+const Cosmic = require('cosmicjs')
+const BootBot = require('bootbot')
+require('dotenv').config()
+const chrono = require('chrono-node')
+var schedule = require('node-schedule')
+const EventEmitter = require('events').EventEmitter
+var config = {}
+const reminders = []
+const eventEmitter = new EventEmitter()
+app.set('port', (process.env.PORT || 5000))
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+app.get('/', function(req, res) {
+  res.send("hey there boi")
+})
+app.get('/webhook/', function(req, res) {
+  if (req.query['hub.verify_token'] === process.env.VERIFY_TOKEN){
+    return res.send(req.query['hub.challenge'])
+  }
+  res.send('wrong token')
+})
+app.listen(app.get('port'), function(){
+  console.log('Started on port', app.get('port'))
+})
+
